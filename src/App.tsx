384007506567c7def326fb2bfbd8990a8fd37a97
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-03-16 15:34:12 -0300

    Hotfix: removed unused css module from a removed package

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Home } from './pages/index';
 import 'normalize.css';
 import '@sweetalert2/theme-dark/dark.min.css'; // SweetAlert Dark theme
-import 'react-perfect-scrollbar/dist/css/styles.css';
 
 function App(): JSX.Element {
   return (
